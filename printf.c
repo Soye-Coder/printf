23676ef2d798a47783456a9fa8eee81455ee599e
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2023-03-26 16:15:27 +0100

    fifth commit

--- printf.c
+++ printf.c
@@ -1,141 +1,54 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#include "main.h"
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int _printf(const char *format, ...);
-int _strlen(char *s);
-char *itoa(int num);
-
-#endif /* HOLBERTON_H */
-
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * _printf - prints output according to a format.
- * @format: character string.
+ * _printf - prints output according to a format
+ * @format: format string
  *
- * Return: the number of characters printed (excluding the null byte used
- * to end output to strings)
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-        va_list ap;
-        int i, len = 0;
-        char *str;
+    va_list args;
+    int count = 0;
 
-        va_start(ap, format);
+    va_start(args, format);
 
-        for (i = 0; format[i] != '\0'; i++)
+    while (*format)
+    {
+        if (*format == '%')
         {
-                if (format[i] == '%' && format[i + 1] == 'd')
-                {
-                        int num = va_arg(ap, int);
-                        str = itoa(num);
-                        len += write(1, str, _strlen(str));
-                        i++;
-                        free(str);
-                }
-                else if (format[i] == '%' && format[i + 1] == 'i')
-                {
-                        int num = va_arg(ap, int);
-                        str = itoa(num);
-                        len += write(1, str, _strlen(str));
-                        i++;
-                        free(str);
-                }
-                else if (format[i] == '%' && format[i + 1] == '%')
-                {
-                        len += write(1, "%", 1);
-                        i++;
-                }
-                else
-                {
-                        len += write(1, &format[i], 1);
-                }
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
         }
-
-        va_end(ap);
-
-        return (len);
-}
-
-/**
- * _strlen - returns the length of a string.
- * @s: string.
- *
- * Return: length of a string.
- */
-int _strlen(char *s)
-{
-        int len;
-
-        for (len = 0; s[len] != '\0'; len++)
+        else
         {
-                ;
+            putchar(*format);
+            count++;
         }
+        format++;
+    }
 
-        return (len);
-}
+    va_end(args);
 
-/**
- * itoa - converts an integer to a string.
- * @num: integer.
- *
- * Return: string representation of an integer.
- */
-char *itoa(int num)
-{
-        char *str = malloc(12);
-        int i = 0, neg = 0;
-
-        if (num == 0)
-        {
-                str[i++] = '0';
-        }
-        else if (num < 0)
-        {
-                neg = 1;
-                num = -num;
-        }
-
-        while (num != 0)
-        {
-                str[i++] = (num % 10) + '0';
-                num /= 10;
-        }
-
-        if (neg)
-        {
-                str[i++] = '-';
-        }
-
-        str[i] = '\0';
-
-        str = _strrev(str);
-
-        return (str);
-}
-
-/**
- * _strrev - reverses a string.
- * @s: string.
- *
- * Return: reversed string.
- */
-char *_strrev(char *s)
-{
-        int i, len = _strlen(s);
-        char tmp;
-
-        for (i = 0; i < len / 2; i++)
-        {
-                tmp = s[i];
-                s[i] = s[len - i - 1];
-                s[len - i - 1] = tmp;
-        }
-
-        return (s);
+    return count;
 }
