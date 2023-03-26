49e14c0defe5641b705a7a9fa00c7f2eb068c09e
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2023-03-26 17:07:30 +0100

    fifth commit

--- main.h
+++ main.h
@@ -4,9 +4,21 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_integer(va_list args);
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_integer(va_list arg);
+
+/**
+ * struct format - Struct format
+ *
+ * @fmt: The format
+ * @f: The function associated
+ */
+typedef struct format
+{
+	char *fmt;
+	int (*f)(va_list);
+} format_t;
 
 #endif /* MAIN_H */
--- printf.c
+++ printf.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * _printf - produces output according to a format
- * @format: character string
- *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
- */
 int _printf(const char *format, ...)
 {
     va_list args;
@@ -24,66 +18,36 @@ int _printf(const char *format, ...)
             {
                 case 'c':
                     putchar(va_arg(args, int));
-                    count++;
                     break;
+
                 case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    while (*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
+                    count += printf("%s", va_arg(args, char *));
                     break;
-                }
+
                 case '%':
                     putchar('%');
-                    count++;
                     break;
+
                 case 'd':
                 case 'i':
-                {
-                    int num = va_arg(args, int);
-                    if (num < 0)
-                    {
-                        num = -num;
-                        putchar('-');
-                        count++;
-                    }
-                    int rev = 0, digit;
-                    do
-                    {
-                        digit = num % 10;
-                        rev = rev * 10 + digit;
-                        num = num / 10;
-                    } while (num != 0);
-
-                    do
-                    {
-                        digit = rev % 10;
-                        putchar(digit + '0');
-                        rev = rev / 10;
-                        count++;
-                    } while (rev != 0);
+                    count += printf("%d", va_arg(args, int));
                     break;
-                }
+
                 default:
                     putchar('%');
                     putchar(*format);
-                    count += 2;
                     break;
             }
         }
         else
         {
             putchar(*format);
-            count++;
         }
+
         format++;
+        count++;
     }
 
     va_end(args);
-
     return count;
 }
